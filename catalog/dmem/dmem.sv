232871e5ad53ade08529b84231414ece62d0789c
Tiffany Shum <112588802+tiffanyshum@users.noreply.github.com>
2024-05-11 14:15:46 -0400

    Update dmem.sv
    data memory is adapted for 16 bits

--- catalog/dmem/dmem.sv
+++ catalog/dmem/dmem.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: Prof Rob Marano
+// Engineer: Tiffany Shum, Lani Wang
 // 
 //     Create Date: 2023-02-07
 //     Module Name: dmem
@@ -15,26 +15,18 @@
 
 `timescale 1ns/100ps
 
-module dmem
-// n=bit length of register; r=bit length of addr to limit memory and not crash your verilog emulator
-    #(parameter n = 16, parameter r = 8)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic           clk, write_enable,
-    input  logic [(n-1):0] addr, writedata,
-    output logic [(n-1):0] readdata
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    logic [(n-1):0] RAM[0:(2**r-1)];
 
-    assign readdata = RAM[addr[(n-1):0]]; // word aligned (ignores lower 2 bits of address)
+module dmem(input logic         clk, write_enable,
+            input logic  [15:0] addr, writedata,
+            output logic [15:0] readdata);
 
-    always @(posedge clk) // write on posedge
-        if (write_enable) RAM[addr[(n-1):0]] <= writedata;
-
+    reg  [15:0] RAM[31:0];
+    assign readdata = RAM[addr[15:2]]; // word aligned, ignores lower 2 bits of addr
+    always @(posedge clk)
+        if (write_enable)
+            RAM[addr[15:2]] <= writedata;
 endmodule
+
+
 
 `endif // DMEM
